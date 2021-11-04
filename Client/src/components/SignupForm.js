144d30c7626962082b3fe05be057c973392c5866
God of War <jamel.eggleston@gmail.com>
2021-11-04 00:39:55 -0400

    updated signupform

--- Client/src/components/SignupForm.js
+++ Client/src/components/SignupForm.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/react-hooks';
 
-import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
+
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser] = useMutation(ADD_USER);
+
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +37,12 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
+      
     } catch (err) {
       console.error(err);
       setShowAlert(true);
